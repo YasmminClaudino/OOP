e00c142d564656e6b5d9223876d679863d9826ac
YasmminClaudino <ymclaudino@gmail.com>
2020-03-29 17:27:05 -0300

    First commit
    init new repository to study OOP

---
+++ README.md
@@ -0,0 +1,1 @@
+Repository created to study and practice Object-oriented programming using Java. 
---
+++ src/application/Program.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.println("Enter the mesuares of triangle X");
+		double xa = scan.nextDouble();
+		double xb = scan.nextDouble();
+		double xc = scan.nextDouble();
+		
+		System.out.println("Enter the mesuares of triangle Y");
+		double ya = scan.nextDouble();
+		double yb = scan.nextDouble();
+		double yc = scan.nextDouble();
+		
+		double areax, areay;
+		String max;
+		
+		areax = area(xa,xb,xc);
+		areay = area(ya,yb,yc);
+		
+		max = (areax > areay ? "TRIANGLE X" : "TRIANGLE Y");
+		
+		System.out.println("TRIANGLE X " + areax);
+		System.out.println("TRIANGLE Y " + areay);
+		System.out.println(max);
+		
+}
+	
+	public static double area(double a, double b, double c) {
+		double p, area;
+		
+		p = (a+b+c)/2.0;
+		area = Math.sqrt(p * (p-a) * (p-b) *(p-c));
+		
+		
+		return area;
+		
+		
+	}
+
+}
